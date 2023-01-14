dee4898db59bb8e519e804421835ef832ca85042
Jh Do <dojh99@gmail.com>
2023-01-14 13:36:19 +0900

    fix: Disable torch.use_deterministic_algorithms"

--- bsrecog/utils.py
+++ bsrecog/utils.py
@@ -12,7 +12,7 @@ def seed_everything(seed=42):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.benchmark = False
-    torch.use_deterministic_algorithms(True)
+    # torch.use_deterministic_algorithms(True)
     os.environ["PYTHONHASHSEED"] = str(seed)
 
 
